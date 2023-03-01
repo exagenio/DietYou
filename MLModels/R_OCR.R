daf751d731052dfea7cdc14f3e538005f99f5f3c
shabeer nuhman <shabeer1227@outlook.com>
2023-03-01 13:06:24 +0530

    made possible changes to the ML model

--- MLModels/R_OCR.R
+++ MLModels/R_OCR.R
@@ -15,17 +15,23 @@ text <- ocr(img)
 
 # identifying the words e.g- protein
 protein_regex <- "Protein"
-protein_kcal_regex <- "([0-9]+\\.[0-9]+) g"
+#chnaged kcal to gram
+protein_gram_regex <- "([0-9]+\\.[0-9]+) g"
 protein_kcal_value <- NA
 
-if (grep1(protein_regex, text)) {
+#changed grep1 to grepl
+
+if (grepl(protein_regex, text)) {
   protein_line <- regexpr(protein_regex, text)
   protein_line_end <- regexpr("\n", text[protein_line:length:length(text)]) + protein_line - 1
   protein_line_text <- substr(text, protein_line, protein_line_end)
   
   # identfying the kcal value of protein and storing it in a variable
-  if (grep(protein_gram_regex, protein_line_text)) {
+  #changed grep to grepl
+  if (grepl(protein_gram_regex, protein_line_text)) {
     protein_gram_match <- regmatches(protein_line_text, regexpr(protein_gram_regex, protein_line_text))
     protein_gram_value <- as.numeric(gsub(" g", "", protein_gram_match))
   }
 }
+#added print function 
+print(protein_gram_value)
