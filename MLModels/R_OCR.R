81a2b3ec2af443ebc852d25d4abaeaf9be3d1619
Vindiya Wickramathunga <vindiyawickramathunga@gmail.com>
2023-03-06 10:27:51 +0530

    edited code, identifies values as well

--- MLModels/R_OCR.R
+++ MLModels/R_OCR.R
@@ -14,52 +14,23 @@ dfo_processed <- dfo %>%
 text <- tesseract::ocr(dfo_processed)
 
 # Extract the gram value in front of the word "protein"
-protein_gram <- NULL
-for (i in seq_along(text)) {
-  if (grepl("protein", tolower(text[i]))) {
-    protein_gram <- gsub("[^0-9.]", "", tolower(text[i+1]))
-    break
-  }
-}
-protein_gram
+protein_gram <- gsub("[^0-9.]", "", 
+                     tolower(gsub(",", "", regmatches(text, regexpr("\\bprotein.*[0-9] g", tolower(text))))[1]))
 
 # Print the extracted gram value
-if (!is.null(protein_gram)) {
-  print(paste("The gram value in front of the word protein is:", protein_gram))
-} else {
-  print("The word protein was not found in the extracted text.")
-}
+print(paste("The gram value in front of the word protein is:", protein_gram))
 
-# Extract the gram value in front of the word "Carbohydrates"
-carbohydrate_gram <- NULL
-for (i in seq_along(text)) {
-  if (grepl("carbohydrates", tolower(text[i]))) {
-    carbohydrate_gram <- gsub("[^0-9.]", "", tolower(text[i+1]))
-    break
-  }
-}
-carbohydrate_gram
+# Extract the gram value in front of the word "carbohydrates"
+carbohydrates_gram <- gsub("[^0-9.]", "", 
+                           tolower(gsub(",", "", regmatches(text, regexpr("\\bcarbohydrates.*[0-9] g", tolower(text))))[1]))
 
 # Print the extracted gram value
-if (!is.null(carbohydrate_gram)) {
-  print(paste("The gram value in front of the word Carbohydrates is:", carbohydrate_gram))
-} else {
-  print("The word Carbohydrates was not found in the extracted text.")
-}
+print(paste("The gram value in front of the word Carbohydrates is:", carbohydrates_gram))
 
-# Extract the gram value in front of the word "Fat, Total"
-fats_gram <- NULL
-for (i in seq_along(text)) {
-  if (grepl("fat, total", tolower(text[i]))) {
-    fats_gram <- gsub("[^0-9.]", "", tolower(text[i+1]))
-    break
-  }
-}
-fats_gram
+# Extract the gram value in front of the word "carbohydrates"
+fats_gram <- gsub("[^0-9.]", "", 
+                           tolower(gsub(",", "", regmatches(text, regexpr("\\fats,total.*[0-9] g", tolower(text))))[1]))
 
 # Print the extracted gram value
-if (!is.null(fats_gram)) {
-  print(paste("The gram value in front of the word Fat is:", fats_gram))
-} else {
-  print("The word Fat was not found in the extracted text.")
-}
+print(paste("The gram value in front of the word Fats is:", fats_gram))
+
