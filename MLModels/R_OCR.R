89215a5eeccf67e0d5e22085e62f80ffa47fd0f1
Vindiya Wickramathunga <vindiyawickramathunga@gmail.com>
2023-03-08 18:04:18 +0530

    Fixed to detect energy/calorie value per serving

--- MLModels/R_OCR.R
+++ MLModels/R_OCR.R
@@ -1,36 +1,43 @@
-install.packages("tesseract")
-install.packages("magick")
-
-library(magick)
 library(tesseract)
+library(magick)
+library(stringr)
 
-dfo <- image_read("calorie_table.png")
+img <- image_read("calorie_table.png")
 
-dfo_processed <- dfo %>%
-  image_convert(type = 'Grayscale') %>%
-  image_trim(fuzz = 40) %>%
-  image_write(format = 'png', density= '300x300') %>%
+img_processed <- image_resize(img, "1000x")
+img_processed <- image_contrast(img_processed)
+img_processed <- image_convert(img_processed, "gray")
+img_processed
 
-text <- tesseract::ocr(dfo_processed)
+text <- ocr(img_processed)
 
-# Extract the gram value in front of the word "protein"
-protein_gram <- gsub("[^0-9.]", "", 
-                     tolower(gsub(",", "", regmatches(text, regexpr("\\bprotein.*[0-9] g", tolower(text))))[1]))
+lines <- str_split(text, "\n")
+lines
 
-# Print the extracted gram value
-print(paste("The gram value in front of the word protein is:", protein_gram))
+# Finding the line with either of the words "Energy" or "Calories"
+energy_line_index <- grep("Energy|Calories", lines)
 
-# Extract the gram value in front of the word "carbohydrates"
-carbohydrates_gram <- gsub("[^0-9.]", "", 
-                           tolower(gsub(",", "", regmatches(text, regexpr("\\bcarbohydrates.*[0-9] g", tolower(text))))[1]))
+# Extracting the energy or calorie values from the line
+energy_str <- lines[[energy_line_index]]
+energy_vals <- ifelse(grepl("Energy", energy_str), 
+                      str_extract(energy_str, "(?<=Energy\\s)[0-9]+"),
+                      str_extract(energy_str, "(?<=Calories\\s)[0-9]+"))
+energy_vals_numeric <- as.numeric(energy_vals)
+energy_vals_numeric
 
-# Print the extracted gram value
-print(paste("The gram value in front of the word Carbohydrates is:", carbohydrates_gram))
+energy_vals_no_na <- na.omit(energy_vals_numeric)
+energy_vals_no_na
 
-# Extract the gram value in front of the word "fats"
-fats_gram <- gsub("[^0-9.]", "", 
-                           tolower(gsub(",", "", regmatches(text, regexpr("\\total fat.*[0-9] g", tolower(text))))[1]))
+# Printing the energy value per serving
+cat("Energy values:", energy_vals_no_na[1], "\n")
 
-# Print the extracted gram value
-print(paste("The gram value in front of the word Fats is:", fats_gram))
+# Getting the index of the line containing words "Serving size"
+serving_line_index <- grep("Serving size", lines)
 
+# Extracting the serving size value from the line
+serving_str <- lines[[serving_line_index]]
+serving_size <- str_extract(serving_str, "\\d+\\.\\d+")
+serving_size_numeric <- as.numeric(serving_size)
+
+# Print the serving size
+cat("Serving size:", serving_size_numeric, "g\n")
