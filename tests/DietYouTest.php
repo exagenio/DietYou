7a0ab5eb719b0507e98f11a5e5a35ddb3578cc54
shabeer nuhman <shabeer1227@outlook.com>
2023-03-09 18:42:35 +0530

    overweight test added

--- tests/DietYouTest.php
+++ tests/DietYouTest.php
@@ -7,11 +7,17 @@ include 'backend/functions.php';
             // $result = bmiCalculate(180,60);
             $this -> assertEquals(18.51851851851852, bmiCalculate(180,60) );
         }
+        
         public function testTEEMale(){
             $this -> assertEquals(1986, TEE(65,180,1.2,"M",25));
         }
+
         public function testTEEFemale(){
             $this -> assertEquals(1786.8, TEE(65,180,1.2,"F",25));
+        }
+
+        public function testTEEOverWeight(){
+            $this -> assertEquals(2,406, TEE(100,180,1.2,"M",25));
         }
     }
 ?>
