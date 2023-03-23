fa5a082c26cbe89bd096e71d7d1ee988fbf228f1
shabeer nuhman <shabeer1227@outlook.com>
2023-03-23 13:07:13 +0530

    made chnages to the asset folder

--- home.php
+++ home.php
@@ -8,8 +8,8 @@
     <title>DietYou</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/feather-icons/dist/feather.min.js"></script>
-    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
-    <link rel="stylesheet" href="frontend/assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+    
 </head>
 
 <body>
