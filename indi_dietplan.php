e707f246297114c4c3358c585ec338808772ffd3
Ranvinu Hewage <123268275+rhewage3@users.noreply.github.com>
2023-03-25 14:18:50 +0530

    diet plan ingredients

---
+++ indi_dietplan.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diet Plan</title>
+    <link rel="stylesheet" href="assets/css/indi_dietplan.css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="heading">
+            <h1>CHEESE COTTAGE LOW FAT & SODIUM</h1>
+            <h3>92g</h3>
+        </div>
+        <div class="ingredient">
+            <h2>INGREDIENTS:</h2>
+        </div>
+    </div>
+    
+</body>
+</html>
