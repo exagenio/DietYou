d3987afe9fb6436efd343b9b006ed87a31a64a00
exagenio <123676288+exagenio@users.noreply.github.com>
2023-04-04 14:41:51 +0530

    create ingredients and exercise pages

---
+++ exercises.php
@@ -0,0 +1,60 @@
+<?php
+include "classes/User.php";
+session_start();
+if ((isset($_SESSION['username'])) && $_SESSION["userVerified"] == 1) {
+    //logged in
+  } else {
+    // Session variable is not set
+  echo'<script>window.location.replace("http://localhost/dietYou/login.php");</script>';
+  }
+include "backend/db.php"; 
+if(isset($_GET['id'])){
+    $exId = $_GET['id'];
+    $username = $_SESSION['username'];
+
+    //get the user data by creating a user object
+    $user = new User($username, $connection);
+    $ncds = $user->getNcds();
+    if($ncds == null){
+        echo "dasdd";
+    }
+
+    $query = "SELECT * FROM exercises WHERE id = '$exId'";
+        //get the result according to the query
+    $result = mysqli_query($connection, $query);
+    if(!$result){
+        die("error");
+    }
+    $exercise = mysqli_fetch_assoc($result);
+
+    $ncdRestrict = explode(",",$exercise["restriction"]);
+    if(in_array($ncds, $ncdRestrict)){
+        die("error");
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Diet Plan</title>
+    <link rel="stylesheet" href="assets/css/indi_dietplan.css">
+</head>
+<body>
+
+    <div class="container">
+        <div class="heading">
+            <h1><?php echo $exercise["name"]; ?></h1>
+            <h3><?php echo $exercise["time"]?> min</h3>
+        </div>
+        <div class="ingredient">
+            <?php 
+            echo "<img src='assets/img/Exercices/".$exId.".gif' alt='".$exercise["name"]."'";
+            ?>
+        </div>
+    </div>
+    
+</body>
+</html>
--- ingredients.php
+++ ingredients.php
@@ -1,13 +1,41 @@
 <?php 
+    include "backend/db.php"; 
     $foodId = $_GET['food'];
     $ratio = $_GET['ratio'];
 if(isset($_GET['food']) && isset($_GET['ratio'])){
     $foodId = $_GET['food'];
     $ratio = $_GET['ratio'];
-    echo $foodId,"<br>",$ratio;
+    $query = "SELECT food_code, name, ingredient_description, ingredient_weight_g, country FROM ingredients WHERE food_code = $foodId";
+     //get the result according to the query
+    $result = mysqli_query($connection, $query);
+
+    // Initialize an empty array to store the rows of the filtered food table
+    $ingredients = array();
+
+    // Fetch each row of data and add it to the array
+    while ($row = mysqli_fetch_assoc($result)) {
+        array_push($ingredients, $row);
+    }
+    if(count($ingredients) == 0 ){
+        die("no ingredients -  error");
+    }
+
+    $query = "SELECT weight FROM foods WHERE food_code =$foodId";
+    $result = mysqli_query($connection, $query);
+    if(!$result){
+        die("error");
+    }
+    $weight = mysqli_fetch_assoc($result);
+    $weightR = $weight["weight"]*round($ratio, 1);
+    $unit;
+    if($ingredients[0]["country"] == "usa"){
+        $unit = "g";
+    }else{
+        $unit = "portion";
+    }
 }
 ?>
-<!-- <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -20,13 +48,20 @@ if(isset($_GET['food']) && isset($_GET['ratio'])){
 
     <div class="container">
         <div class="heading">
-            <h1>CHEESE COTTAGE LOW FAT & SODIUM</h1>
-            <h3>92g</h3>
+            <h1><?php echo $ingredients[0]["name"]; ?></h1>
+            <h3><?php echo $weightR?>g</h3>
         </div>
         <div class="ingredient">
             <h2>INGREDIENTS:</h2>
+            <ul>
+            <?php
+            foreach ($ingredients as $ingredient) {
+                echo "<li>".$ingredient["ingredient_description"]." - ".$ingredient["ingredient_weight_g"]*$ratio.$unit."</li>";
+            }
+            ?>
+            </ul>
         </div>
     </div>
     
 </body>
-</html> -->
+</html>
